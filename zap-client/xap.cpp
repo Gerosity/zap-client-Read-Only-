5328e358724525e1da26996001029ade2da364dd
Gerosity <110794161+Gerosity@users.noreply.github.com>
2024-04-01 20:09:36 +0100

    Updated to game version v3.0.62.30

--- zap-client/xap.cpp
+++ zap-client/xap.cpp
@@ -596,7 +596,7 @@ int main(int argc, char *argv[])
     std::this_thread::sleep_for(std::chrono::seconds(1));
     std::cout << "By Gerosity" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "For Game Version v3.0.62.29" << std::endl;
+    std::cout << "For Game Version v3.0.62.23" << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
     // Initialize Overlay Window //
