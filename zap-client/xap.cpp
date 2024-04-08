e43371c718ea17de144a3448a5059a8eb19ca4d5
Gerosity <110794161+Gerosity@users.noreply.github.com>
2024-04-08 19:32:56 +0100

    1.0.6 update

--- zap-client/xap.cpp
+++ zap-client/xap.cpp
@@ -371,7 +371,7 @@ void RenderUI()
         TitleText << "zap-client";
         Date << OverlayWindow.currentDateTime(2);
         Time << OverlayWindow.currentDateTime(3);
-        Version << "Version 1.0.5";
+        Version << "Version 1.0.6";
         UnknownCheats << "unknowncheats.me";
         Spacings << "  |  ";
         std::string combined = TitleText.str() + Spacings.str() + Date.str() + Spacings.str() + Time.str() + Spacings.str() + Version.str() + Spacings.str() + UnknownCheats.str();
@@ -592,11 +592,11 @@ int main(int argc, char *argv[])
     std::cout << "                    ░░░░░                                                                   " << std::endl;
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "zap client - ver 1.0.5 [Read Only]" << std::endl;
+    std::cout << "zap client - ver 1.0.6 [Read Only]" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
     std::cout << "By Gerosity" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "For Game Version v3.0.62.23" << std::endl;
+    std::cout << "For Game Version v3.0.63.32" << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
     // Initialize Overlay Window //
