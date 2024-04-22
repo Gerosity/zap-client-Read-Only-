c665e2b79d3eb66dd6cad31eaf77028b2081de43
Gerosity <110794161+Gerosity@users.noreply.github.com>
2024-04-22 19:21:34 +0100

    Updated Offsets To Latest Version

--- zap-client/Core/Offsets.hpp
+++ zap-client/Core/Offsets.hpp
@@ -1,25 +1,25 @@
 #pragma once
 //https://www.unknowncheats.me/forum/apex-legends/319804-apex-legends-reversal-structs-offsets.html
  
-//GameVersion = v3.0.63.32
+//GameVersion = v3.0.65.42
 
 // Core    
 constexpr long OFF_REGION = 0x140000000;                      //[Static]->Region
-constexpr long OFF_LEVEL = 0x16f9500;                         //[Miscellaneous]->LevelName
+constexpr long OFF_LEVEL = 0x16f94f0;                         //[Miscellaneous]->LevelName
 constexpr long OFF_LOCAL_PLAYER = 0x21830f8;                  //[Miscellaneous]->LocalPlayer
-constexpr long OFF_ENTITY_LIST = 0x1DD45F8;                   //[Miscellaneous]->cl_entitylist              ???
+constexpr long OFF_ENTITY_LIST = 0x1DD45F8;                   //[Miscellaneous]->cl_entitylist
 constexpr long OFF_NAME_INDEX = 0x38;                         //nameIndex
 constexpr long OFF_NAME_LIST = 0xC5FF080;                     //[Miscellaneous]->NameList
 constexpr long OFF_GAMEMODE = 0x021ba960;                     //mp_gamemode
 constexpr long OFF_SQUAD_ID = 0x0334;                          //m_squadID
  
 // HUD
-constexpr long OFF_VIEWRENDER = 0x73f1970;                    //[Miscellaneous]->ViewRenderer
+constexpr long OFF_VIEWRENDER = 0x73f1978;                    //[Miscellaneous]->ViewRenderer
 constexpr long OFF_VIEWMATRIX = 0x11a350;                     //[Miscellaneous]->ViewMatrix
  
 // Buttons
-constexpr long OFF_INATTACK = 0x073f2bf8;                     //[Buttons]->in_attack
-constexpr long OFF_IN_JUMP = 0x073f2d00;                      //[Buttons]->in_jump
+constexpr long OFF_INATTACK = 0x073f2c18;                     //[Buttons]->in_attack
+constexpr long OFF_IN_JUMP = 0x073f2cf8;                      //[Buttons]->in_jump
 constexpr long OFF_IN_DUCK = 0x073f2df8;                      //[Buttons]->in_duck
 constexpr long OFF_IN_FORWARD = 0x073f2b48;                //[Buttons]->in_forward
 constexpr long OFF_TRAVERSAL_START_TIME = 0x2af0;          //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
@@ -66,10 +66,10 @@ constexpr long OFF_TIME_BASE = 0x2088;                        //m_currentFramePl
 constexpr long OFF_WEAPON_DISCARDED = 0x1599;                 //[RecvTable.DT_WeaponX]->m_discarded
 constexpr long OFF_VIEWMODELS = 0x2d18;                       //m_hViewModels
  
-//glow
+// Glow
 constexpr long OFF_HIGHLIGHT_TYPE_SIZE = 0x34;                //                         
-constexpr long OFF_GLOW_ENABLE = 0x26c;                       //[DT_HighlightSettings].        
-constexpr long OFF_GLOW_THROUGH_WALL = 0x26c;                 //[DT_HighlightSettings].       
+constexpr long OFF_GLOW_ENABLE = 0x26c;                       //Script_Highlight_GetCurrentContext
+constexpr long OFF_GLOW_THROUGH_WALL = 0x26c;                 //Script_Highlight_SetVisibilityType     
 constexpr long OFF_GLOW_FIX = 0x268;                        //                                      
 constexpr long OFF_GLOW_HIGHLIGHT_ID = 0x28C;                 //[DT_HighlightSettings].m_highlightServerActiveStates     
 constexpr long OFF_GLOW_HIGHLIGHTS = 0xBB247D0;                //highlightsettings
